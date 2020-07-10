85cfbe0227c4de7360e54f29de8662cb26b5dc1d
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-10 12:16:03 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,24 +1,30 @@
 def find_item_by_name_in_collection(name, collection)
-  item = nil
- collection.each do |key, info|
- if key[:item] == name
-   item = key
- end
- end
- item
-end
+ count = 0 
+ while count < collection.length 
+ if collection[count][:item] == name 
+   return collection[count]
+ end 
+ count += 1 
+ end 
+ end 
 
 def consolidate_cart(cart)
-  new_cart = {}
-  cart.each do |hash|
-    hash.each do |key, info|
-      if new_cart[key]
-        new_cart[key][:count] += 1 
-      else
-        new_cart[key] = info 
-        new_cart[key][:count] = 1
-      end
-    end
-  end
-  new_cart
-end
+ new_cart = []
+ count = 0 
+ while count < cart.length 
+ new_item = find_item_by_name_in_collection(cart[count][:item], new_cart)
+ if new_item
+   new_item[:count] += 1 
+ else 
+   new_item = {
+     :item => cart[count][:item],
+     :price => cart[count][:price],
+     :clearance => cart[count][:clearance],
+     :count => 1 
+   }
+   new_cart << new_item 
+ end 
+ count =+ 1 
+ end 
+ new_cart
+ end 
